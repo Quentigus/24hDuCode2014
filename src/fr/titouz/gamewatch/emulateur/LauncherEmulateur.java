67da4e2ed0e90085faa8df55b2a804a869187a0f
boiteux <gaelcarre@gmail.com>
2014-01-25 12:55:42 +0100

    Mise en place de l'écran de l'emulateur

--- src/fr/titouz/gamewatch/emulateur/LauncherEmulateur.java
+++ src/fr/titouz/gamewatch/emulateur/LauncherEmulateur.java
@@ -1,5 +1,10 @@
 package fr.titouz.gamewatch.emulateur;
 
+import fr.titouz.gamewatch.emulateur.controller.MainController;
+
 public class LauncherEmulateur {
 
+	public static void main (String[] args) {
+		MainController.getInstance().init();
+	}
 }
