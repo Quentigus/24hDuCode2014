3362d9af375359b6cdd6e3ce9a835b904336f3da
AydenSekey <ad.duroy@gmail.com>
2014-01-26 03:44:03 +0100

    Ajout d'entete GPL manquantes.

--- src/fr/titouz/gamewatch/modeleur/modele/GTransition.java
+++ src/fr/titouz/gamewatch/modeleur/modele/GTransition.java
@@ -1,3 +1,19 @@
+/*Copyright ANDRU Bastien, CARRE, Gaël DUROY Adrien, GOSSELIN Quentin, JARROT Kathleen
+ * (25/01/2014)
+ * This file is part of Titz & Watch.
+ * 
+ * Titz & Watch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * Titz & Watch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Titz & Watch.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fr.titouz.gamewatch.modeleur.modele;
 
 import java.io.Serializable;
