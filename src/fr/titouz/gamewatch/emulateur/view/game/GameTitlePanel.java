f9a91db432f00c746762f60b1f0308b8b7e9ef2a
Adrien <ad.duroy@gmail.com>
2014-02-03 14:37:28 +0100

    Ajout des entêtes de licence manquante.

--- src/fr/titouz/gamewatch/emulateur/view/game/GameTitlePanel.java
+++ src/fr/titouz/gamewatch/emulateur/view/game/GameTitlePanel.java
@@ -1,11 +1,25 @@
+/* Copyright ANDRU Bastien, CARRE, Gaël DUROY Adrien, GOSSELIN Quentin, JARROT Kathleen
+ * (25/01/2014)
+ * This file is part of Titz & Watch.
+ * 
+ * Titz & Watch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * Titz & Watch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Titz & Watch.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fr.titouz.gamewatch.emulateur.view.game;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.IOException;
-import java.util.Date;
 
 import javax.swing.JPanel;
 
